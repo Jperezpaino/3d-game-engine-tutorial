3c3c3bec27cd6bf278f4db82aec55e1937858d1c
Jperezpaino <j.perez.paino@gmail.com>
2025-11-19 16:59:27 +0100

    Initial Code Commit
    Initial Code Commit

---
+++ src/main/java/es/noa/rad/game/Application.java
@@ -0,0 +1,25 @@
+package es.noa.rad.game;
+
+  /**
+   *
+   */
+  public final class Application {
+
+    /**
+     *
+     */
+    private Application() {
+      super();
+      System.out.println("Hello, World!");
+    }
+
+    /**
+     *
+     * @param _arguments {@code String...}
+     */
+    public static void main(
+        final String... _arguments) {
+      new Application();
+    }
+
+  }
